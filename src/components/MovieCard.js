e2498d04caa8013006768cf76c6989295d7e14aa
AyushR97j <ayushraj803110@gmail.com>
2024-01-09 13:19:16 +0530

    User Selected Genre Based Movie Section

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,8 +3,8 @@ import { IMG_CDN_URL } from "../utils/constants";
 const MovieCard = ({ posterPath }) => {
   if (!posterPath) return null;
   return (
-    <div className="w-36 md:w-48 pr-4">
-      <img alt="Movie Card" src={IMG_CDN_URL + posterPath} />
+    <div className="w-36 rounded-lg md:w-52 pr-4 hover:scale-105 transition-transform">
+      <img className="rounded-lg" alt="Movie Card" src={IMG_CDN_URL + posterPath} />
     </div>
   );
 };
